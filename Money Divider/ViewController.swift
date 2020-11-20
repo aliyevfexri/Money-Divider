da962d65f80cdf0821adc8ecdaad63086fa51520
Fakhri <aliyevfexri@yandex.com>
2020-11-20 19:58:59 +0400

    Initial Commit

---
+++ Money Divider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money Divider
+//
+//  Created by Fakhri Aliyev on 20.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
